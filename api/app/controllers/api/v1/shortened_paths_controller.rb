7687169882d966e5b1489e697a836918b0cd353c
nickfarm27 <nicholasfarm27@gmail.com>
2024-10-29 22:32:43 +0800

    Add route that returns the target URL for a short URL

---
+++ api/app/controllers/api/v1/shortened_paths_controller.rb
@@ -0,0 +1,42 @@
+class Api::V1::ShortenedPathsController < ApplicationController
+  def redirect
+    if shortened_url.present?
+      TrackClickJob.perform_later(shortened_url[:id], Time.zone.now, ip_address)
+
+      render json: { target_url: shortened_url[:target_url] }, status: :ok
+    else
+      render json: { errors: ["Shortened path not found"] }, status: :not_found
+    end
+  end
+
+  private
+
+  def shortened_path
+    params[:shortened_path]
+  end
+
+  def ip_address
+    params[:ip_address]
+  end
+
+  def shortened_url
+    @shortened_url ||= fetch_from_cache || fetch_from_database
+  end
+
+  def fetch_from_cache
+    Rails.cache.read("shortened_url/#{shortened_path}")
+  end
+
+  def fetch_from_database
+    shortened_url_record = ShortenedUrl.find_by(path: shortened_path)
+    return nil if shortened_url_record.blank?
+
+    shortened_url_hash = {
+      id: shortened_url_record.id,
+      target_url: shortened_url_record.target_url,
+    }
+
+    Rails.cache.write("shortened_url/#{shortened_path}", shortened_url_hash)
+    shortened_url_hash
+  end
+end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  namespace :api do
+    namespace :v1 do
+      resources :shortened_urls, only: [:create]
+
+      get "shortened_paths/:shortened_path/redirect", to: "shortened_paths#redirect"
+    end
+  end
 end
